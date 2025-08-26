94b2eb8a53e68440438b73d52fdb4d479f90911d
2qx <2qx@users.noreply.github.com>
2025-08-26 19:57:41 +0000

    deploy: 2qx/vox@a86b0d9b0d1c46cdd31bd6ebd59bd24e1d1d32a2

---
+++ _app/immutable/entry/start.3gnb4-Ll.js
@@ -0,0 +1,2 @@
+import{l as o,b as r}from"../chunks/By0X0jQN.js";export{o as load_css,r as start};
+//# sourceMappingURL=start.3gnb4-Ll.js.map
--- _app/immutable/entry/start.BowLHxl1.js
+++
@@ -1,2 +0,0 @@
-import{l as o,b as r}from"../chunks/BddUEcfb.js";export{o as load_css,r as start};
-//# sourceMappingURL=start.BowLHxl1.js.map
