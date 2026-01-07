5d91479c425f7e2bc892ffd17e5cf72f9843c6ec
2qx <2qx@users.noreply.github.com>
2026-01-07 23:09:00 +0000

    deploy: 2qx/vox@148632967f1ab0ea22a9357e1ee7187c14da17a2

---
+++ _app/immutable/chunks/CKvmxwAg.js
@@ -0,0 +1,2 @@
+import{c as t,a}from"./CVas6eta.js";const e=(c=!0)=>t(a(c)),i=e;export{i as c};
+//# sourceMappingURL=CKvmxwAg.js.map
--- _app/immutable/entry/start.CzuRgLWw.js
+++
@@ -1,2 +0,0 @@
-import{l as o,b as r}from"../chunks/CGm_U7Ao.js";export{o as load_css,r as start};
-//# sourceMappingURL=start.CzuRgLWw.js.map
