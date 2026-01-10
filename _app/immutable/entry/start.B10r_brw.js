026cf73aa96de3469f022a3974f1bf3f1e3d5698
2qx <2qx@users.noreply.github.com>
2026-01-10 14:58:56 +0000

    deploy: 2qx/vox@10ff797a1b7bd6e8d6bc6627293a2633a235256d

--- _app/immutable/entry/start.B10r_brw.js
+++
@@ -1,2 +0,0 @@
-import{l as o,b as r}from"../chunks/qlHsE7qy.js";export{o as load_css,r as start};
-//# sourceMappingURL=start.B10r_brw.js.map
---
+++ _app/immutable/entry/start.CRCQ6hUO.js
@@ -0,0 +1,2 @@
+import{l as o,b as r}from"../chunks/67zWOrQL.js";export{o as load_css,r as start};
+//# sourceMappingURL=start.CRCQ6hUO.js.map
