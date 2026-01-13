d8080e4eb13c20352d2b8696ea130777d410b599
2qx <2qx@users.noreply.github.com>
2026-01-13 15:32:02 +0000

    deploy: 2qx/vox@544a1f6d2437bc25efbe4e112d8f933492782208

---
+++ _app/immutable/entry/start.DaB3H8aN.js
@@ -0,0 +1,2 @@
+import{l as o,b as r}from"../chunks/D961Voyx.js";export{o as load_css,r as start};
+//# sourceMappingURL=start.DaB3H8aN.js.map
--- _app/immutable/entry/start.DkX-U5DK.js
+++
@@ -1,2 +0,0 @@
-import{l as o,b as r}from"../chunks/CGSs__62.js";export{o as load_css,r as start};
-//# sourceMappingURL=start.DkX-U5DK.js.map
