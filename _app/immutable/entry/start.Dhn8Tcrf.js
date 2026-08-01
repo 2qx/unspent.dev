e623979d40c760ef11133fe03795383d18e16c6c
2qx <2qx@users.noreply.github.com>
2026-08-01 10:59:20 +0000

    deploy: 2qx/vox@7b990a16cc012d466c4edc74e92affb0dc22625b

--- _app/immutable/entry/start.Dhn8Tcrf.js
+++
@@ -1,2 +0,0 @@
-import{l as o,b as r}from"../chunks/CfOtOsVg.js";export{o as load_css,r as start};
-//# sourceMappingURL=start.Dhn8Tcrf.js.map
---
+++ _app/immutable/entry/start.sz1McmpN.js
@@ -0,0 +1,2 @@
+import{l as o,b as r}from"../chunks/CvgK0vKI.js";export{o as load_css,r as start};
+//# sourceMappingURL=start.sz1McmpN.js.map
